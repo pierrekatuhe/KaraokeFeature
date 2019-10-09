1e853db168bd62ef474a731b237fd3e7db1622e4
Pierre Katuhe <pierrekatuhe@gmail.com>
2019-10-09 18:44:22 +0700

    Initial commit

---
+++ KaraokeFeature/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(212, 59)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Test Extend Screen"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 77)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(212, 59)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Test Send Text to Other PC"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 142)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(212, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(45, 169)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(236, 224)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button3 As Button
+End Class
---
+++ KaraokeFeature/Form1.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+
+Public Class Form1
+    Dim m_listener As TcpListener
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form2.Location = Screen.AllScreens(UBound(Screen.AllScreens)).Bounds.Location + New Point(100, 100)
+        Form2.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        WriteData("Coba yang lain", "127.0.0.1")
+    End Sub
+
+    Public Sub WriteData(ByVal data As String, ByRef IP As String)
+        Console.WriteLine("Sending message """ & data & """ to " & IP)
+        Dim client As TcpClient = New TcpClient()
+        client.Connect(New IPEndPoint(IPAddress.Parse(IP), 8080))
+        Dim stream As NetworkStream = client.GetStream()
+        Dim sendBytes As Byte() = Encoding.ASCII.GetBytes(data)
+        stream.Write(sendBytes, 0, sendBytes.Length)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim localAddr As IPAddress = IPAddress.Parse("127.0.0.1")
+        m_listener = New TcpListener(localAddr, 8080)
+        m_listener.Start()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim bytes(300000) As Byte
+        Dim data As String = ""
+
+        Console.Write("Waiting for a connection... ")
+
+        Dim client As TcpClient = m_listener.AcceptTcpClient()
+        Dim requesterIP As String = client.Client.RemoteEndPoint.ToString().Split(New Char() {":"})(0)
+
+        Console.WriteLine("Connected!")
+
+        data = ""
+        Dim stream As NetworkStream = client.GetStream()
+
+        Dim BytesRead As Integer = 0
+        BytesRead = stream.Read(bytes, 0, bytes.Length)
+        data = Encoding.ASCII.GetString(bytes, 0, BytesRead)
+        TextBox1.Text = data
+
+
+
+    End Sub
+End Class
